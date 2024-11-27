4eb7a608715741cd3b274fa81d37a38d203a99dd
Pranav Vashi <neobuddy89@gmail.com>
2024-11-28 00:44:30 +0530

    iconloaderlib: Do not wrap themed icons
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/570
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- iconloaderlib/src/com/android/launcher3/icons/BaseIconFactory.java
+++ iconloaderlib/src/com/android/launcher3/icons/BaseIconFactory.java
@@ -55,6 +55,8 @@ import java.lang.annotation.Retention;
  */
 public class BaseIconFactory implements AutoCloseable {
 
+    public static final int CONFIG_HINT_NO_WRAP = 0x1000000;
+
     private static final int DEFAULT_WRAPPER_BACKGROUND = Color.WHITE;
     private static final float LEGACY_ICON_SCALE = .7f * (1f / (1 + 2 * getExtraInsetFraction()));
 
@@ -221,19 +223,21 @@ public class BaseIconFactory implements AutoCloseable {
             // Need to convert to Adaptive Icon with insets to avoid cropping.
             tempIcon = createShapedAdaptiveIcon(bitmapDrawable.getBitmap());
         }
-        AdaptiveIconDrawable adaptiveIcon = normalizeAndWrapToAdaptiveIcon(tempIcon, scale);
-        Bitmap bitmap = createIconBitmap(adaptiveIcon, scale[0],
+        tempIcon = normalizeAndWrapToAdaptiveIcon(tempIcon, scale);
+        Bitmap bitmap = createIconBitmap(tempIcon, scale[0],
                 options == null ? MODE_WITH_SHADOW : options.mGenerationMode);
         int color = (options != null && options.mExtractedColor != null)
                 ? options.mExtractedColor : ColorExtractor.findDominantColorByHue(bitmap);
         BitmapInfo info = BitmapInfo.of(bitmap, color);
 
-        if (adaptiveIcon instanceof Extender extender) {
+        if (tempIcon instanceof Extender extender) {
             info = extender.getExtendedInfo(bitmap, color, this, scale[0]);
-        } else if (IconProvider.ATLEAST_T && mThemeController != null && adaptiveIcon != null) {
+
+        } else if (IconProvider.ATLEAST_T && mThemeController != null && tempIcon != null
+                && tempIcon instanceof AdaptiveIconDrawable) {
             info.setThemedBitmap(
                     mThemeController.createThemedBitmap(
-                        adaptiveIcon,
+                        (AdaptiveIconDrawable)tempIcon,
                         info,
                         this,
                         options == null ? null : options.mSourceHint
@@ -241,7 +245,7 @@ public class BaseIconFactory implements AutoCloseable {
             );
         }
         FlagOp flagOp = getBitmapFlagOp(options);
-        if (adaptiveIcon instanceof WrappedAdaptiveIcon) {
+        if (tempIcon instanceof WrappedAdaptiveIcon) {
             flagOp = flagOp.addFlag(BitmapInfo.FLAG_WRAPPED_NON_ADAPTIVE);
         }
         info = info.withFlags(flagOp);
@@ -351,14 +355,18 @@ public class BaseIconFactory implements AutoCloseable {
     }
 
     @Nullable
-    protected AdaptiveIconDrawable normalizeAndWrapToAdaptiveIcon(
+    protected Drawable normalizeAndWrapToAdaptiveIcon(
             @Nullable Drawable icon, @NonNull final float[] outScale) {
         if (icon == null) {
             return null;
         }
-
+        if ((icon.getChangingConfigurations() & CONFIG_HINT_NO_WRAP) == 0) {
+            AdaptiveIconDrawable adaptiveIcon;
+            adaptiveIcon = wrapToAdaptiveIcon(icon);
+            icon = adaptiveIcon;
+        }
         outScale[0] = ICON_VISIBLE_AREA_FACTOR;
-        return wrapToAdaptiveIcon(icon);
+        return icon;
     }
 
     /**
